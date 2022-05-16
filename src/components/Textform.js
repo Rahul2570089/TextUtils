0ce4fb8ba5ace1511c49e2cb77eda1444764afc8
rahul <rs2570089@gmail.com>
2022-05-16 10:52:44 +0530

    TextUtils added

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -23,22 +23,21 @@ export default function Textform(props) {
         setText(newtext);
     }
 
-    let b = text.length === 0 ? 0 : text.split(" ").length
 
     return (
         <>
-            <div className="container">
+            <div className="container my-2">
                 <h1 style={{color: props.mode === 'dark' ? 'white' : 'black'}}>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className={`form-control text-${props.mode === 'dark' ? 'light' : 'dark'}`} id="mybox" rows="8" value={text} onChange={handleonchange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white'}}></textarea>
-                    <button className='btn btn-primary my-3' onClick={handleclickupper}>Convert to Uppercase</button>
-                    <button className="btn btn-primary mx-3" onClick={handleclicklower}>Convert to Lowercase</button>
-                    <button className="btn btn-primary" onClick={handleclear}>Clear text</button>
+                    <textarea className="form-control" id="mybox" rows="8" value={text} onChange={handleonchange} style={{backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode==='dark' ? 'white' : '#042743'}}></textarea>
+                    <button disabled={text.length===0} className='btn btn-primary mx-1 my-3' onClick={handleclickupper}>Convert to Uppercase</button>
+                    <button disabled={text.length===0} className="btn btn-primary mx-1 my-3" onClick={handleclicklower}>Convert to Lowercase</button>
+                    <button disabled={text.length===0} className="btn btn-primary mx-1 my-3" onClick={handleclear}>Clear text</button>
                 </div>
             </div>
-            <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
+            <div className="container my-1" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
                 <h1 className='my-4'>Text summary</h1>
-                <p>{b} words</p>
+                <p>{text.split(" ").filter((element) => {return element.length!==0}).length} words</p>
                 <p>{text.length} characters</p>
             </div>
         </>
